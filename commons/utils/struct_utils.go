e6befb0a3c7f9dcd8d5b4376c8b6d05776d9f417
mark-jiang <mark.jiang@greenpacket.com.cn>
2020-07-20 11:00:03 +0800

    v1.0.0-feature:add Struct Copy

---
+++ commons/utils/struct_utils.go
@@ -0,0 +1,58 @@
+package utils
+
+import "reflect"
+
+func DeepFields(interfaceType reflect.Type) []reflect.StructField {
+	var fields []reflect.StructField
+
+	for i := 0; i < interfaceType.NumField(); i++ {
+		v := interfaceType.Field(i)
+		if v.Anonymous && v.Type.Kind() == reflect.Struct {
+			fields = append(fields, DeepFields(v.Type)...)
+		} else {
+			fields = append(fields, v)
+		}
+	}
+
+	return fields
+}
+func StructCopy(DstStructPtr interface{}, SrcStructPtr interface{}) {
+	srcv := reflect.ValueOf(SrcStructPtr)
+	dstv := reflect.ValueOf(DstStructPtr)
+	srct := reflect.TypeOf(SrcStructPtr)
+	dstt := reflect.TypeOf(DstStructPtr)
+	if srct.Kind() != reflect.Ptr || dstt.Kind() != reflect.Ptr ||
+		srct.Elem().Kind() == reflect.Ptr || dstt.Elem().Kind() == reflect.Ptr {
+		panic("Fatal error:type of parameters must be Ptr of value")
+	}
+	if srcv.IsNil() || dstv.IsNil() {
+		panic("Fatal error:value of parameters should not be nil")
+	}
+	srcV := srcv.Elem()
+	dstV := dstv.Elem()
+	srcfields := DeepFields(reflect.ValueOf(SrcStructPtr).Elem().Type())
+	for _, v := range srcfields {
+		if v.Anonymous {
+			continue
+		}
+		dst := dstV.FieldByName(v.Name)
+		src := srcV.FieldByName(v.Name)
+		if !dst.IsValid() {
+			continue
+		}
+		if src.Type() == dst.Type() && dst.CanSet() {
+			dst.Set(src)
+			continue
+		}
+		if src.Kind() == reflect.Ptr && !src.IsNil() && src.Type().Elem() == dst.Type() {
+			dst.Set(src.Elem())
+			continue
+		}
+		if dst.Kind() == reflect.Ptr && dst.Type().Elem() == src.Type() {
+			dst.Set(reflect.New(src.Type()))
+			dst.Elem().Set(src)
+			continue
+		}
+	}
+	return
+}
