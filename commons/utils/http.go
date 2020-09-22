0bccf37b07a0c167295fa052ea8792a0b6ba31f6
mark-jiang <mark.jiang@greenpacket.com.cn>
2020-09-22 09:24:14 +0800

    v1.0.0-feature: adjust proxy request performance

--- commons/utils/http.go
+++ commons/utils/http.go
@@ -18,9 +18,13 @@ type BaseResponse struct {
 	Time int64           `json:"time"`
 }
 
+type ProxyRequestHeader struct {
+	ContentType string
+}
+
 var TimeOut = time.Second * 10
 
-func ProxyRequest(method string, header http.Header, url string, body []byte) (response []byte, respHeader fasthttp.ResponseHeader, err error) {
+func ProxyRequest(method string, header http.Header, url string, body []byte) (response []byte, respHeader ProxyRequestHeader, err error) {
 	req := fasthttp.AcquireRequest()
 	defer fasthttp.ReleaseRequest(req)
 	req.SetBody(body)
@@ -32,14 +36,14 @@ func ProxyRequest(method string, header http.Header, url string, body []byte) (r
 	req.Header.SetMethod(method)
 	req.Header.Set(fasthttp.HeaderConnection, fasthttp.HeaderKeepAlive)
 	req.SetRequestURI(url)
-	resp := &fasthttp.Response{}
-
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
 	if err := fasthttp.DoTimeout(req, resp, time.Second*5); err != nil {
 		slog.Infof("Http Request Do Error %s", err.Error())
-		return nil, fasthttp.ResponseHeader{}, err
+		return nil, ProxyRequestHeader{}, err
 	}
 
-	return resp.Body(), resp.Header, nil
+	return resp.Body(), ProxyRequestHeader{ContentType: string(resp.Header.ContentType())}, nil
 
 }
 func Request(method string, url string, body interface{}, response interface{}) (code int, err error) {
