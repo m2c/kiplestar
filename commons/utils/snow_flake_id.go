72992005f7e78088953d02787f561e5f57e06dc8
GitTsewell <18008072409@163.com>
2020-11-05 17:34:32 +0800

    v1.0.0-feature:GetSnowflakeId

---
+++ commons/utils/snow_flake_id.go
@@ -0,0 +1,67 @@
+package utils
+
+import (
+	"math"
+	"strconv"
+	"time"
+)
+
+var (
+	//machineID     int64 // 机器 id 占10位, 十进制范围是 [ 0, 1023 ]
+	sn            int64 // 序列号占 12 位,十进制范围是 [ 0, 4095 ]
+	lastTimeStamp int64 // 上次的时间戳(毫秒级), 1秒=1000毫秒, 1毫秒=1000微秒,1微秒=1000纳秒
+)
+
+func init() {
+	lastTimeStamp = time.Now().UnixNano() / 1000000
+}
+
+func GetSnowflakeId(machineID int64) int64 {
+	curTimeStamp := time.Now().UnixNano() / 1000000
+
+	// 同一毫秒
+	if curTimeStamp == lastTimeStamp {
+		sn++
+		// 序列号占 12 位,十进制范围是 [ 0, 4095 ]
+		if sn > 4095 {
+			time.Sleep(time.Millisecond)
+			curTimeStamp = time.Now().UnixNano() / 1000000
+			lastTimeStamp = curTimeStamp
+			sn = 0
+		}
+
+		// 取 64 位的二进制数 0000000000 0000000000 0000000000 0001111111111 1111111111 1111111111  1 ( 这里共 41 个 1 )和时间戳进行并操作
+		// 并结果( 右数 )第 42 位必然是 0,  低 41 位也就是时间戳的低 41 位
+		rightBinValue := curTimeStamp & 0x1FFFFFFFFFF
+		// 机器 id 占用10位空间,序列号占用12位空间,所以左移 22 位; 经过上面的并操作,左移后的第 1 位,必然是 0
+		rightBinValue <<= 22
+		id := rightBinValue | machineID | sn
+		return id
+	}
+
+	if curTimeStamp > lastTimeStamp {
+		sn = 0
+		lastTimeStamp = curTimeStamp
+		// 取 64 位的二进制数 0000000000 0000000000 0000000000 0001111111111 1111111111 1111111111  1 ( 这里共 41 个 1 )和时间戳进行并操作
+		// 并结果( 右数 )第 42 位必然是 0,  低 41 位也就是时间戳的低 41 位
+		rightBinValue := curTimeStamp & 0x1FFFFFFFFFF
+		// 机器 id 占用10位空间,序列号占用12位空间,所以左移 22 位; 经过上面的并操作,左移后的第 1 位,必然是 0
+		rightBinValue <<= 22
+		id := rightBinValue | machineID | sn
+		return id
+	}
+
+	if curTimeStamp < lastTimeStamp {
+		return 0
+	}
+
+	return 0
+}
+
+func StringToDecimal(str string) (float64, error) {
+	f64, err := strconv.ParseFloat(str, 10)
+	if err != nil {
+		return f64, err
+	}
+	return math.Trunc(f64*1e2+0.5) * 1e-2, nil
+}
