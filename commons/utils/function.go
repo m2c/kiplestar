5093730f7b43f15d806eed964c5f241ffa34b976
mark-jiang <mark.jiang@greenpacket.com.cn>
2020-11-25 09:34:27 +0800

    v1.0.0-feature: add function to string

--- commons/utils/function.go
+++ commons/utils/function.go
@@ -126,3 +126,26 @@ func CommonsService(parms BaseParams, f func(parms BaseParams) (interface{}, err
 type BaseParams struct {
 	info string
 }
+
+func ToString(from interface{}) string {
+	v := reflect.ValueOf(from)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	switch v.Kind() {
+	case reflect.String:
+		return v.String()
+	case reflect.Bool:
+		if v.Bool() {
+			return "true"
+		}
+		return "false"
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(v.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return strconv.FormatUint(v.Uint(), 10)
+	case reflect.Float32, reflect.Float64:
+		return strconv.FormatFloat(v.Float(), 'f', -1, 64)
+	}
+	return ""
+}
