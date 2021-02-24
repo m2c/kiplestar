e628a73e82e5fe343415fb4343bce1735dbfc1e9
illidan <empty>
2021-02-24 11:44:10 +0800

    v1.0.0-fixbug: move server_ids file to commons package

---
+++ commons/server_ids.go
@@ -0,0 +1,16 @@
+package commons
+
+const (
+	RazerpayGatewayServiceId ResponseCode = 11 * 1e5
+	GkspgServiceId           ResponseCode = 12 * 1e5
+	GkccServiceId            ResponseCode = 13 * 1e5
+	GkfcServiceId            ResponseCode = 14 * 1e5
+	GksettlementServiceId    ResponseCode = 15 * 1e5
+	GkuserServiceId          ResponseCode = 16 * 1e5
+	GkwServiceId             ResponseCode = 17 * 1e5
+	GkseServiceId            ResponseCode = 18 * 1e5
+	GkmeosServiceId          ResponseCode = 19 * 1e5
+	GklogServiceId           ResponseCode = 20 * 1e5
+	GkfpxServiceId           ResponseCode = 21 * 1e5
+	GkrpServiceId            ResponseCode = 22 * 1e5
+)
