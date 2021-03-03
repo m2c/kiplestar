2b5ba2364254b8e9d84e6ff0392799e8a98f5869
illidan <empty>
2021-03-03 17:38:02 +0800

    v1.0.8-feature: add function RegisterCodeAndMsg

--- commons/commons_enum.go
+++ commons/commons_enum.go
@@ -35,4 +35,14 @@ func GetCodeAndMsg(code ResponseCode) string {
 	return "{}"
 }
 
+// msg will be used as default msg, and you can change msg with function 'BuildFailedWithMsg' or 'BuildSuccessWithMsg' or 'response.WithMsg' for once.
+func RegisterCodeAndMsg(arr map[ResponseCode]string) {
+	if len(arr) == 0 {
+		return
+	}
+	for k, v := range arr {
+		CodeMsg[k] = v
+	}
+}
+
 const X_REQUEST_ID = "X-Request-Id"
