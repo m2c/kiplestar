3d408ed0aedd42f481af50199c33642d18a36ea2
GitTsewell <18008072409@163.com>
2021-03-09 15:39:16 +0800

    v1.0.14-feature: Add InitCustomPath

--- config/server_config.go
+++ config/server_config.go
@@ -2,10 +2,11 @@ package config
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
 var SC *ServerConfig
@@ -64,6 +65,49 @@ func Init() {
 			Configs = InitAllConfig("application-test.yaml")
 		} else if strings.EqualFold(env, "prod") {
 			Configs = InitAllConfig("application-prod.yaml")
+		}
+	}
+	fmt.Printf("config %+v", Configs)
+	SC = conf
+}
+
+func InitCustomPath(path string) {
+	yamlFile, err := ioutil.ReadFile(fmt.Sprintf("%s/application.yaml", path))
+	if err != nil {
+		panic(fmt.Errorf("load application.yaml error, will exit,please fix the application"))
+	}
+	conf := &ServerConfig{}
+	err = yaml.Unmarshal(yamlFile, conf)
+	if err != nil {
+		panic(err)
+	}
+	env := conf.SConfigure.Profile
+	for _, v := range os.Args {
+		arg := strings.Split(v, "=")
+		if len(arg) != 2 {
+			continue
+		}
+		if arg[0] == "env" {
+			if arg[1] != "dev" && arg[1] != "test" && arg[1] != "prod" {
+				panic(fmt.Errorf("command env %s need dev/test/prod", arg[1]))
+			}
+			env = arg[1]
+		}
+	}
+	/*
+	* parse the config file
+	 */
+
+	if len(env) == 0 {
+		// load dev profile application-dev.yaml
+		Configs = InitAllConfig(fmt.Sprintf("%s/application-dev.yaml", path))
+	} else {
+		if strings.EqualFold(env, "dev") {
+			Configs = InitAllConfig(fmt.Sprintf("%s/application-dev.yaml", path))
+		} else if strings.EqualFold(env, "test") {
+			Configs = InitAllConfig(fmt.Sprintf("%s/application-test.yaml", path))
+		} else if strings.EqualFold(env, "prod") {
+			Configs = InitAllConfig(fmt.Sprintf("%s/application-prod.yaml", path))
 		}
 	}
 	fmt.Printf("config %+v", Configs)
