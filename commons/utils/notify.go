ccea03ae6e90f8deec1a6debbdf5ee993ac27a13
卓玛 <Laraine.Zhuoma@greenpacket.com.cn>
2021-03-17 17:09:51 +0800

    v1.0.21-feature change email notify

--- commons/utils/notify.go
+++ commons/utils/notify.go
@@ -1,28 +1,24 @@
 package utils
 
 import (
-	"encoding/base64"
 	"fmt"
 	slog "github.com/m2c/kiplestar/commons/log"
 	"net/http"
 )
 
 //send  email with file
-func SendEmail(url, appkey, secret string, data interface{}, fileName string, address, template string) error {
+func SendEmail(url, appKey, secret string, data interface{}, fileName string, address, template string) error {
 	file, err := DataToExcelByte(data)
 	if err != nil {
 		return err
 	}
-	bufStore := make([]byte, base64.StdEncoding.EncodedLen(len(file)))
-	base64.StdEncoding.Encode(bufStore, file)
 
 	content := make(map[string]interface{})
-	content["channelId"] = 1
 	content["templateName"] = template
 	content["mailTo"] = address
-	content["attachFile"] = bufStore
+	content["attachFile"] = file
 	content["attachFileName"] = fileName
-	content["apiKey"] = appkey
+	content["appKey"] = appKey
 	content["secret"] = secret
 
 	code, err := Request(http.MethodPost, url, content, nil, nil)
